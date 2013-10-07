541fb77cc97584236add608296f0c6322c8b09f4
John Rodler <john@jrod.info>
2013-10-06 21:39:29 -0400

    setup initial ng-view animation code, enhanced angular route setup

--- js/app.js
+++ js/app.js
@@ -8,38 +8,38 @@ var YC_App = angular.module('YC_App', ['ngRoute']);
 YC_App.config( function( $routeProvider ) {
 
   $routeProvider.when( '/home',
-      {
-        templateUrl : 'partials/home.html',
-        controller  : 'MainController'
-      })
-    .when( '/health',
-      {
-        templateUrl : 'partials/health.html',
-        controller  : 'MainController'
-      })
-    .when( '/goals',
-      {
-        templateUrl : 'partials/goals.html',
-        controller  : 'MainController'
-      })
-    .when( '/calendar',
-      {
-        templateUrl : 'partials/calendar.html',
-        controller  : 'MainController'
-      })
-    .when( '/community',
-      {
-        templateUrl : 'partials/community.html',
-        controller  : 'MainController'
-      })
-    .otherwise({ redirectTo: '/home' });
+    {
+      templateUrl : 'views/home.html',
+      controller  : 'AppCtrl'
+    })
+  .when( '/health',
+    {
+      templateUrl : 'views/health.html',
+      controller  : 'AppCtrl'
+    })
+  .when( '/goals',
+    {
+      templateUrl : 'views/goals.html',
+      controller  : 'AppCtrl'
+    })
+  .when( '/calendar',
+    {
+      templateUrl : 'views/calendar.html',
+      controller  : 'AppCtrl'
+    })
+  .when( '/community',
+    {
+      templateUrl : 'views/community.html',
+      controller  : 'AppCtrl'
+    })
+  .otherwise({ redirectTo: '/home' });
 });
 
-YC_App.controller('MainController', function( /* $scope */ ) {
+YC_App.controller('AppCtrl', function( /* $scope */ ) {
   // TODO
 });
 
-YC_App.controller('NavController', function( $scope, $location ) {
+YC_App.controller('NavCtrl', function( $scope, $location ) {
     $scope.isActive = function( route ) {
       return route === $location.path();
     };
